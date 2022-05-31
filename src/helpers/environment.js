c3ba36d45f466bf452f9dbd36b8d3d97ca889e03
JakeMaz <jakemazanowski@gmail.com>
2022-05-31 18:16:59 -0400

    changed APIURL to heroku server

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -6,7 +6,7 @@ switch (window.location.hostname){
         APIURL = 'http://localhost:3069';
         break;
     case 'gamestashapp-client.herokuapp.com':
-        APIURL = 'https://gamestashapp-client.herokuapp.com'
+        APIURL = 'https://gamestash-app.herokuapp.com'
 }
 
 export default APIURL;
